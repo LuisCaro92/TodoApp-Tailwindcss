9ad06f90fd59639b7760fc599337387dda4fe6bf
luis caro <luis.carosalgado1@gmail.com>
2023-07-11 22:57:33 -0400

    cambiando errores

---
+++ src/componentes/register/Register.js
@@ -0,0 +1,11 @@
+
+
+const Register =()=> {
+    return(
+        <div className="w-100 mb-40 pb-80 bg-slate-500 "> 
+        <h1 className="text-left text-5xl font-caprasimo font-bold tracking-widest>Register">Register</h1>
+        </div>
+    )
+}
+
+export default Register
