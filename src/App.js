95a6fb23f24ea0d3f6b966a7b59e8cf6ad943296
luis caro <luis.carosalgado1@gmail.com>
2023-07-12 00:17:08 -0400

    creando register

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import Home from "./componentes/Home";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Register from "./componentes/register/Register";
 
 const App =() => {
   return(
-<Home/>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path ="/" element = { <Home/>}/>
+        <Route path ="/login" element = { <Register/> }/>
+        <Route/>
+      </Routes>
+      </BrowserRouter>
+   
+    </div>
+
   )
 }
 
--- src/componentes/register/Register.js
+++ src/componentes/register/Register.js
@@ -1,9 +1,24 @@
 
 
-const Register =()=> {
-    return(
-        <div className="w-100 mb-40 pb-80 bg-slate-500 "> 
-        <h1 className="text-left text-5xl font-caprasimo font-bold tracking-widest>Register">Register</h1>
+const Register = () => {
+    return (
+        <div className="bg-gray-800  justify-center min-h-screen py-40">
+            <div className="mx-auto  flex container-center ">
+               <div className="flex w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
+                <div className="w-1/2">
+                    <img className="flex max-w-lg" src="https://media.istockphoto.com/id/1433239683/es/foto/fondo-rosa-azul-y-p%C3%BArpura-sunset.jpg?s=170667a&w=0&k=20&c=jh3E-8QLL-aPze4Pi1fKnP5UXmhzjcw4J37Vi31F7DY=" alt="Image-register"/>
+                    <h1 className="mb-30">Welocme</h1>
+                    <div>
+                        <p>
+                            Bienvenido a App-Todo-List. afadfaadsadv adsfad fadfag  ajfnjaf aosdjfjad aoiddjajf adfaf
+                        </p>
+                    </div>
+                </div>  
+                <div className="w-1/2">
+                    <h2>Register</h2>
+                </div>
+               </div>
+            </div>
         </div>
     )
 }
