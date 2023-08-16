cd702a0f6532a10a7c56a0f613ee01a19a77eb03
luis caro <luis.carosalgado1@gmail.com>
2023-08-15 22:33:58 -0400

    agegando cambios

--- src/componentes/card/Card.js
+++ src/componentes/card/Card.js
@@ -40,11 +40,11 @@ const products = [
   
   
     return (
-      <div className="bg-gray-400">
+      <div className="bg-gray-400 rounded-lg">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="sr-only">Products</h2>
   
-          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 xl:gap-x-8">
             {products.map((product) => (
               <a key={product.id} href={product.href} className="group">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-400 xl:aspect-h-8 xl:aspect-w-7">
