da0b6130fa4fd4b783340389b6a30992db7656e4
luis caro <luis.carosalgado1@gmail.com>
2023-08-15 22:17:16 -0400

    agregados las cards

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -1,17 +1,18 @@
+import { useState } from "react";
 import Card from "./card/Card";
 
-fetch
+
 
 
 const  Home=()=> {
 
+ 
   return (
 
     <div className="bg-gray-800 min-h-screen h-full text-yellow-400 flex items-center justify-center py-20 px-5">
-      <Card>
+ 
+    <Card/>
 
-      </Card>
-       
     </div>
   );
 }
