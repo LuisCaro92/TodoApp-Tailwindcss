a635fccc9a1d79cb67287f62f0951b3d33084999
luis caro <luis.carosalgado1@gmail.com>
2023-08-19 23:29:09 -0400

    se realizara un cambio completo para cambiar de tecnologia

---
+++ src/componentes/profile/Datos.js
@@ -0,0 +1,15 @@
+
+
+
+const Datos =() => {
+
+
+    return(
+        <div className="container items-center">
+            <div ></div>
+            
+        </div>
+    )
+}
+
+export default Datos
--- src/componentes/profile/Profile.js
+++ src/componentes/profile/Profile.js
@@ -3,7 +3,7 @@ export default function Example() {
       < div className="items-center bg-gray-200 h-400 ">
         <div className="flex-space-x-1 overflow-hidden ">
           <img
-            className="inline-block h-100 w-100 rounded-full ring-2 ring-white"
+            className="ml-10  inline-block h-100 w-100 rounded-full ring-2 ring-white"
             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
             alt=""
           />
